22311d49206bc6963c5494daf255974b180707ab
wangzx <1132154046@qq.com>
2020-11-23 20:54:07 +0800

    🎨  Init

---
+++ vue-hoc/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <Fetch>
+    <template #default="data">{{ data.data.title }}</template>
+    <template #loading>Loading...</template>
+  </Fetch>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import Fetch from './Hoc/Fetch.vue'
+
+export default defineComponent({
+  components: {
+    Fetch,
+  },
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue-hoc/src/components/Hoc/Fetch.vue
@@ -0,0 +1,43 @@
+<template>
+  <slot v-if="data" :data="data"></slot>
+  <slot v-if="loading" name="loading"></slot>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+import { ref, toRefs } from 'vue'
+
+export default {
+  setup() {
+    const fetchApi = () => {
+      const data = ref(null)
+      const loading = ref(false)
+
+      const fetchData = async () => {
+        loading.value = true
+        try {
+          data.value = await (await fetch('https://jsonplaceholder.typicode.com/todos/1')).json()
+          loading.value = false
+        } catch (err) {
+          loading.value = false
+          throw new Error(err)
+        }
+      }
+      fetchData()
+      return {
+        data,
+        loading,
+      }
+    }
+
+    const { data, loading } = toRefs(fetchApi())
+    console.log(data, loading)
+    return {
+      data,
+      loading,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
